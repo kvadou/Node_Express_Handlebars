d8b3bececf407565fd2c1c62ef7c8de817c1effa
DPK <dougkvamme@gmail.com>
2021-01-09 14:38:17 -0600

    added seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Olive Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Slider', false);
