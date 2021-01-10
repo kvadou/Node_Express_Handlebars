1f036cdf39675955bdf9aab9fc8bc63aa95eca52
DPK <dougkvamme@gmail.com>
2021-01-09 19:53:23 -0600

    added burger.js

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+// Import the ORM.js
+let orm = require("../config/orm.js");
+
+let burger = {
+  all: function (cb) {
+    orm.all("burgers", function (res) {
+      cb(res);
+    });
+  },
+  create: function (cols, vals, cb) {
+    orm.create("burgers", cols, vals, function (res) {
+      cb(res);
+    });
+  },
+  update: function (objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function (res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the database
+module.exports = burger;
