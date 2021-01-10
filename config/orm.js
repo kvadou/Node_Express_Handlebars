14436698033a034342cd926959ee8feaba1e9484
DPK <dougkvamme@gmail.com>
2021-01-09 19:51:02 -0600

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,22 +1,14 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
-
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
 
-// Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
 
@@ -39,7 +31,7 @@ function objToSql(ob) {
   return arr.toString();
 }
 
-// Object for all our SQL statement functions.
+// selectAll
 var orm = {
   all: function (tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
@@ -50,6 +42,8 @@ var orm = {
       cb(result);
     });
   },
+
+  // insertOne
   create: function (table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -70,7 +64,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: double cheeseburger, devour: true}
+  // updateOne
   update: function (table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -90,5 +84,5 @@ var orm = {
   },
 };
 
-// Export the orm object for the model (burger.js).
+// module.exports
 module.exports = orm;
